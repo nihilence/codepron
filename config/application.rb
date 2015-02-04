2561027ce684a1fa2982a1e27d7e18ce989db962
Sabice <heads90@gmail.com>
2015-02-04 01:57:43 -0800

    heroking

--- config/application.rb
+++ config/application.rb
@@ -5,9 +5,9 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+config.assets.initialize_on_precompile = false
 
 module CodePron
   class Application < Rails::Application
-    config.assets.initialize_on_precompile = false
   end
 end
