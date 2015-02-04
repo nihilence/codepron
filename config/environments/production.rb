5d2000ed1f4083b8dc7196d5e755903d200eb2be
Sabice <heads90@gmail.com>
2015-02-04 02:27:33 -0800

    herokuing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,8 @@ CodePron::Application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  config.assets.initialize_on_precompile = false
+  
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
