97a24c0837d453d7b44ac981fad046f86b2bf0bd
Sabice <heads90@gmail.com>
2015-02-08 12:04:40 -0800

    seed trials

--- db/seeds.rb
+++ db/seeds.rb
@@ -187,3 +187,191 @@ Preview.create(title:"Feisty Pins",
   div{position:absolute;}
  EOF
 )
+
+js_text = <<-EOF
+/* polyfill requestAnimationFrame
+* taken from:
+*   https://gist.github.com/paulirish/1579671
+* then minified with:
+*   http://closure-compiler.appspot.com/
+*/
+(function(){for(var e=0,b=["ms","moz","webkit","o"],a=0;a<b.length&&!window.requestAnimationFrame;++a)window.requestAnimationFrame=window[b[a]+"RequestAnimationFrame"],window.cancelAnimationFrame=window[b[a]+"CancelAnimationFrame"]||window[b[a]+"CancelRequestAnimationFrame"];window.requestAnimationFrame||(console.log("fill"),window.requestAnimationFrame=function(a,b){var c=(new Date).getTime(),d=Math.max(0,16-(c-e)),f=window.setTimeout(function(){a(c+d)},d);e=c+d;return f});window.cancelAnimationFrame||(window.cancelAnimationFrame=function(a){clearTimeout(a)})})()
+
+
+// start the actual code
+
+// some basic variables for later use
+var particles = {};
+var particleIndex = 0;
+var particleCount = 10;
+var mouse = {};
+
+
+/**
+* @class Canvas
+* @description
+*   allow creating a canvas
+*/
+function Canvas(){
+  var _this = this;
+  _this.canvas = document.createElement('canvas');
+  _this.context = _this.canvas.getContext('2d');
+}
+
+/**
+* @function Canvas.init
+* @description
+*   sets the basic properties of a black fullscreen canvas
+*/
+Canvas.prototype.init = function(){
+  var _this = this;
+  var style = _this.canvas.style;
+  style.background = '#000';
+  style.position = 'absolute';
+  style.cursor = 'none';
+  style.top = 0;
+  style.left = 0;
+  document.body.appendChild(_this.canvas);
+}
+
+/**
+* @function Canvas.scale
+* @description
+*   make the canvas fullscreen
+*/
+Canvas.prototype.scale = function(){
+  var _this = this;
+  _this.canvas.height = window.innerHeight;
+  _this.canvas.width = window.innerWidth;
+}
+
+/**
+* @class Particle
+* @description
+*   allow creating particles
+*/
+function Particle(c){
+  var _this = this;
+  _this.canvas = c;
+  _this.x = mouse.x || _this.canvas.canvas.width / 2;
+  _this.y = mouse.y || _this.canvas.canvas.height / 2;
+  _this.velocityX = Math.random() * 6 - 3;
+  _this.velocityY = Math.random() * 6 - 3;
+  _this.size = ~~(Math.random()*4) + 1;
+  _this.age = 0;
+  _this.death = Math.random() * 50 + 10;
+  _this.gravity = Math.random() + 0.1;
+  particleIndex++;
+  _this.id = particleIndex;
+  particles[particleIndex] = _this;
+
+  };
+
+  /**
+  * @function Particle.draw
+  * @description
+  *   draws the actual particles
+  */
+  Particle.prototype.draw = function(){
+    var _this = this;
+    _this.canvas.context.globalCompositeOperation = 'lighter';
+    _this.x += _this.velocityX;
+    _this.y += _this.velocityY;
+    _this.velocityY += _this.gravity;
+    _this.velocityX += _this.gravity;
+
+    // let's do some random stuff to the direction
+    if(Math.random() <= 0.5) {
+      _this.velocityY = Math.random() * 10 - 5
+      _this.velocityX = Math.random() * 10 - 5;
+    }
+
+    // everybody must die some time
+    // particles randomly die after a while
+    _this.age++;
+    if (_this.age >= _this.death) {
+      delete particles[_this.id];
+    }
+
+    // we also want to randomly change the color
+    _this.canvas.context.fillStyle = 'hsla(' +
+    ~~(Math.random() * 180 - 50) +
+    ',100%,50%,0.3)';
+    // a particle is actually a circle/arc
+    _this.canvas.context.beginPath();
+    _this.canvas.context.arc(_this.x,_this.y,_this.size,0,Math.PI*2,false);
+    _this.canvas.context.fill();
+    };
+
+
+
+
+
+
+
+    // let's create a canvas
+    var c = new Canvas;
+    c.init();
+
+
+
+
+
+    /**
+    * @function run
+    * @description
+    *   will provide an interval like function with
+    *   requestAnimationFrame to draw the particles
+    */
+    function run(){
+      c.context.globalCompositeOperation = 'source-over';
+      c.context.fillStyle = 'rgba(0,0,0,0.1)';
+      c.context.fillRect(0,0,c.canvas.width,c.canvas.height);
+
+      // let's create some particles
+      for (var i = 0; i < particleCount; i++) {
+        new Particle(c);
+      }
+      // and draw each one seperately
+      for (var i in particles) {
+        particles[i].draw();
+      }
+      requestAnimationFrame(run);
+    }
+    /**
+    * @function scaleCanvas
+    * @description
+    *   allow to scale the canvas to fill the window
+    */
+    function scaleCanvas(){
+      c.scale();
+    }
+    /**
+    * @function followMouse
+    * @description
+    *   make the particles follow the mouse
+    */
+    function followMouse(e){
+      mouse.x = e.pageX;
+      mouse.y = e.pageY;
+    }
+    /**
+    * @function forgetMouse
+    * @description
+    *   forget the mouse data
+    */
+    function forgetMouse(e){
+      mouse.x = false;
+      mouse.y = false;
+    }
+
+
+    // init stuff
+    scaleCanvas();
+    run();
+    // the canvas is always fullscreen
+    window.addEventListener('resize', scaleCanvas);
+    c.canvas.addEventListener('mousemove', followMouse);
+    c.canvas.addEventListener('mouseleave', forgetMouse);
+  EOF
+Preview.create(title:"Follow the Mouse", html:"<div></div>", js: js_text)
