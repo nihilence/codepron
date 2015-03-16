64aac7ba17ec83773b235704942ba8db3c958bd0
Sabice <heads90@gmail.com>
2015-03-16 07:43:34 -0700

    require ssl

--- config/application.rb
+++ config/application.rb
@@ -10,5 +10,6 @@ Bundler.require(*Rails.groups)
 module CodePron
   class Application < Rails::Application
     config.assets.initialize_on_precompile = false
+    config.force_ssl = true
   end
 end
